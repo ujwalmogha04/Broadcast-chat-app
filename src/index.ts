574eaa95d8607b1fd73bdbdd828edbb1a4a206ae
ujwal <ujwalmogha2004@gmail.com>
2024-12-01 21:59:44 +0530

    Added 2 way communication

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port : 8080});
+const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
 
-wss.on("connection" , (socket) =>{
-       userCount = userCount + 1;
-       console.log("User Connected " , userCount);
+wss.on("connection", (socket) => {
+    userCount = userCount + 1;
+    console.log("User Connected ", userCount);
+    socket.on("message", (message) => {
+        console.log(message.toString() + "received from user");
+        setTimeout(() => {
+            socket.send(message.toString() + "sent from server");
+        }, 2000)
+
+    })
 })
