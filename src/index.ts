c15647e76e30c302cbf71cb295523198b20857ee
ujwal <ujwalmogha2004@gmail.com>
2024-12-03 13:17:29 +0530

    Added room logic

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,58 @@
-import { all } from "axios";
 import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+let rooms: { [room: string]: WebSocket[] } = {};
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("User Connected ", userCount);
+
     socket.on("message", (message) => {
-        console.log(message.toString() + "received from user");
-        allSockets.forEach((s) => {
-            s.send(message.toString());
-        })
+        console.log("A user connected");
+        try {
+            const parsedMessage = JSON.parse(message as unknown as string);
+
+            if (parsedMessage.type == "join") {
+                const roomId = parsedMessage.payload.roomId;
+
+                if (!rooms[roomId]) {
+                    rooms[roomId] = []
+                }
+
+                rooms[roomId].push(socket);
+                console.log(`User joined room: ${roomId}`);
+            }
+
+            if (parsedMessage.type == "chat") {
+                const roomId = parsedMessage.payload.roomId;
+                const messageContent = parsedMessage.payload.message;
+
+                if (rooms[roomId]) {
+                    rooms[roomId].forEach((userSocket) => {
+                        userSocket.send(JSON.stringify({
+                            type: "chat",
+                            payload: { message: messageContent }
+                        }))
+
+                    })
+                    console.log(`Message sent to room ${roomId}: ${messageContent}`);
+                }
+            }
+        }
+        catch (error) {
+            console.error("Error processing message:", error);
+        }
+
     })
 
-    socket.on("disconnet", () => {
-        allSockets = allSockets.filter((x) => x != socket);
+    socket.on("close", () => {
+        for (let room in rooms) {
+            rooms[room] = rooms[room].filter((userSocket) =>  userSocket !== socket );
+
+            if (rooms[room].length === 0) {
+                delete rooms[room];
+                console.log("room is deleted")
+            }
+        }
+        console.log("user Disconnected");
     })
 })
